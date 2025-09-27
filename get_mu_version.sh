743699c8d3594f320e9d0a29e587c09766a04698
3e/\e BoBa <57889575+ZeleB0BA@users.noreply.github.com>
2025-09-27 19:18:53 +0500

    Update get_mu_version.sh

--- get_mu_version.sh
+++ get_mu_version.sh
@@ -26,11 +26,11 @@ if [ -f $out_file ]
 then 
     rm $out_file 
 fi
+
 touch $out_file
     echo "===="$heading "на "$cur_date, $cur_time "====" >$out_file
     echo "Версия ЛМ ЧЗ на сайте: [[$lmchez_site | $lmchez_from_site]]" >>$out_file
     echo "^  № маг  ^  версия ЛМЧЗ  ^  статус  ^  нужно скачивать  ^  режим работы  ^  обновление  ^  синхронизация  ^" >>$out_file
-
 # Full LMCHEZ list
 for i in 01 02 03 04 05 07 08 09 10 11 13 14 16 21 22 24 25 26 27 28 29 31 32 33 66 68 86
 #for i in 01
@@ -38,25 +38,16 @@ for i in 01 02 03 04 05 07 08 09 10 11 13 14 16 21 22 24 25 26 27 28 29 31 32 33
     Num=$i
     echo "--- $Num ------------------------------------------------------------------------"
     echo -n "^  [[http://$Host$Num.$Domain:8000/ |  $Num]]  |  " >>$out_file
-
-
     curl -X GET "http://$Host$Num:$Port$PathStatus" -H "accept: application/json" > $get_temp
     echo -e ""  >> $get_temp
-
-
-
     echo -e "" >> $get_temp
-
     version=$(jq -r '.version' $get_temp)
-
     status=$(jq -r '.status' $get_temp)
-
     requiresDownload=$(jq -r '.requiresDownload' $get_temp)
-
     operationMode=$(jq -r '.operationMode' $get_temp)
     lastUpdate=$(jq -r '.lastUpdate' $get_temp)
     lastSync=$(jq -r '.lastSync' $get_temp)
-fixme=""
+    fixme=""
     if [[ $version != "$lmchez_from_site" ]]
     then
       fixme=":!:"
